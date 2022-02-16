36ae6b396cb32c2313f77e0e19e41df1fe86aff0
piyush-soni <piyushsoni.210@gmail.com>
2022-02-16 13:15:10 +0530

    coding guideline changes

--- js/nm_notice_data.js
+++ js/nm_notice_data.js
@@ -1,71 +1,68 @@
 window.addEventListener('load', function () 
 	{
-		var toggle = 0;
-		var noti_id = 1;
+		var zero_notices_present = true;
+		var notices_id = 1;
 		var nm_container = document.querySelector("#nm_container");
 
 		function nm_add_to_container(nm_notice)
 		{
-			if(toggle == 0)
+			if(zero_notices_present)
 			{
 				document.querySelector('#nm_container h3').remove();
-				toggle = 1;
+				zero_notices_present = false;
 			}
 			
-			var nm_container_div = '<div id="all_nm_'+noti_id+'"></div><br>';
-			nm_container.innerHTML += nm_container_div;
+			nm_container.innerHTML += '<div id="all_nm_'+notices_id+'"></div><br>';
 
-			var temp = '#all_nm_'+noti_id;
-			var nm_box = document.querySelector(temp);
-			nm_notice.classList.add("inline");
-			nm_notice.classList.add("notice-alt");
+			var nm_notice_div_id = '#all_nm_'+notices_id;
+			var nm_div_box = document.querySelector(nm_notice_div_id);
+
+			nm_notice.classList.add("inline","notice-alt");
 			nm_notice.style.visibility = "unset";
-			nm_box.appendChild(nm_notice);
-			noti_id++;
+			
+			nm_div_box.appendChild(nm_notice);
+			notices_id++;
 		}
 
-		function nm_sanatize(nm_all_notice)
+		function nm_sanitize(nm_notices)
 		{
-			for(var i=0; i<nm_all_notice.length; i++)
+			for(var i=0; i<nm_notices.length; i++)
 			{
-				if(nm_all_notice[i].classList.contains('hidden') || nm_all_notice[i].hasAttribute("aria-hidden"))
-				{
-					continue;
-				}
-				else
-				{
-					nm_add_to_container(nm_all_notice[i]);
-				}
+				if(nm_notices[i].classList.contains('hidden') || nm_notices[i].hasAttribute("aria-hidden"))
+					{continue;}
+				nm_add_to_container(nm_notices[i]);
 			}
 		}
-
 		
-		nm_sanatize(document.querySelectorAll(".notice.notice-error"));
-		nm_sanatize(document.querySelectorAll(".error"));
-		nm_sanatize(document.querySelectorAll(".notice.notice-success"));
-		nm_sanatize(document.querySelectorAll(".updated"));
-		nm_sanatize(document.querySelectorAll(".notice.notice-warning"));
-		nm_sanatize(document.querySelectorAll(".notice.notice-info"));
-
-		var all_notices = document.querySelectorAll(".notice");
-
-		for(var i=0; i<all_notices.length; i++)
+		if(nm_container != null)
 		{
-			if(all_notices[i].classList.contains("notice-success") 
-				|| all_notices[i].classList.contains("notice-error") 
-				|| all_notices[i].classList.contains("notice-warning")
-				|| all_notices[i].classList.contains("notice-info")
-				|| all_notices[i].classList.contains("hidden")
-				|| all_notices[i].classList.contains("updated")
-				|| all_notices[i].classList.contains("error")
-				|| all_notices[i].hasAttribute('aria-hidden'))
+			nm_sanitize(document.querySelectorAll(".notice.notice-error"));
+			nm_sanitize(document.querySelectorAll(".error"));
+			nm_sanitize(document.querySelectorAll(".notice.notice-success"));
+			nm_sanitize(document.querySelectorAll(".updated"));
+			nm_sanitize(document.querySelectorAll(".notice.notice-warning"));
+			nm_sanitize(document.querySelectorAll(".notice.notice-info"));
+
+			var all_notices = document.querySelectorAll(".notice");
+
+			for(var i=0; i<all_notices.length; i++)
 			{
-				continue;
-			}
-			else
-			{
+				if(all_notices[i].classList.contains("notice-success") 
+					|| all_notices[i].classList.contains("notice-error") 
+					|| all_notices[i].classList.contains("notice-warning")
+					|| all_notices[i].classList.contains("notice-info")
+					|| all_notices[i].classList.contains("hidden")
+					|| all_notices[i].classList.contains("updated")
+					|| all_notices[i].classList.contains("error")
+					|| all_notices[i].hasAttribute('aria-hidden'))
+					{continue;}
+				
 				nm_add_to_container(all_notices[i]);
 			}
+		}
+		else
+		{
+			document.getElementById("wpfooter").innerHTML += '<style>.notice {visibility:unset;}.updated {visibility:unset;}.error {visibility:unset;}</style>';
 		}
 
 	}
