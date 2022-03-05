22c102d72fee3bff889bf77b7c7cd3089e25fb8c
Mayank <goyalmayank522@gmail.com>
2022-03-05 10:56:02 +0530

    wordpress coding guidelines followed

--- js/nm_notice_data.js
+++ js/nm_notice_data.js
@@ -6,16 +6,16 @@ window.addEventListener('load', function ()
 		let nm_container_unread = document.querySelector("#nm_container_unread");
 		let nm_hash_of_read_notices_json = JSON.stringify(nm_hash_of_read_notices);
 		let number_of_notifications = 0;
-		let number_of_unread_notifications = 0;
+ 		let number_of_unread_notifications = 0;
 
-		function nm_add_to_container(nm_notice)
+		function nm_add_to_container( nm_notice )
 		{
-			let nm_notice_hash = MD5(nm_notice.innerHTML);
-			if(nm_notice_hash == "d41d8cd98f00b204e9800998ecf8427e"){return;}
-			nm_notice.classList.add("inline","notice-alt","nm-common");
+			let nm_notice_hash = MD5( nm_notice.innerHTML );
+			if ( nm_notice_hash == "d41d8cd98f00b204e9800998ecf8427e" ){ return; }
+			nm_notice.classList.add("inline", "notice-alt", "nm-common");
 			nm_notice.style.visibility = "unset";
 			nm_container_all.appendChild(nm_notice);
-			if(nm_hash_of_read_notices_json.includes(nm_notice_hash)){
+			if ( nm_hash_of_read_notices_json.includes( nm_notice_hash ) ) {
 				nm_notice.classList.add("nm-seen");
 				return;
 			}
@@ -23,23 +23,23 @@ window.addEventListener('load', function ()
 			notices_id++;
 		}
 
-		function nm_sanitize_and_proceed(nm_notices)
+		function nm_sanitize_and_proceed( nm_notices )
 		{
-			for(let notice_number=0; notice_number<nm_notices.length; notice_number++)
+			for ( let notice_number = 0; notice_number < nm_notices.length; notice_number++)
 			{
-				if(nm_notices[notice_number].classList.contains("hidden")
+				if ( nm_notices[notice_number].classList.contains("hidden")
 					|| nm_notices[notice_number].hasAttribute("aria-hidden")
 					|| nm_notices[notice_number].classList.contains("hide-if-js")
-					|| nm_notices[notice_number].classList.contains("inline"))
-					{continue;}
-				nm_add_to_container(nm_notices[notice_number]);
+					|| nm_notices[notice_number].classList.contains("inline") )
+					{ continue; }
+				nm_add_to_container( nm_notices[notice_number] );
 			}
 		}
 
-		if(nm_container != null)
+		if( nm_container != null )
 		{
-			var nm_inline_notices = document.querySelectorAll(".inline");
-			for(var nm_inline_notice=0; nm_inline_notice<nm_inline_notices.length; nm_inline_notice++)
+			let nm_inline_notices = document.querySelectorAll(".inline");
+			for ( let nm_inline_notice = 0; nm_inline_notice < nm_inline_notices.length; nm_inline_notice++)
 			{
 				nm_inline_notices[nm_inline_notice].style.visibility = "visible";
 			}
@@ -52,16 +52,16 @@ window.addEventListener('load', function ()
 
 			let all_notices = document.querySelectorAll(".notice");
 
-			for(let notice_number=0; notice_number<all_notices.length; notice_number++)
+			for ( let notice_number=0; notice_number < all_notices.length; notice_number++ )
 			{
-				if(all_notices[notice_number].classList.contains("nm-common")
+				if ( all_notices[notice_number].classList.contains("nm-common")
 					|| all_notices[notice_number].classList.contains("hidden")
 					|| all_notices[notice_number].hasAttribute("aria-hidden")
 					|| all_notices[notice_number].classList.contains("hide-if-js")
-					|| all_notices[notice_number].classList.contains("inline"))
-					{continue;}
+					|| all_notices[notice_number].classList.contains("inline") )
+					{ continue; }
 				
-				nm_add_to_container(all_notices[notice_number]);
+				nm_add_to_container( all_notices[notice_number] );
 			}
 		}
 		else
@@ -71,17 +71,17 @@ window.addEventListener('load', function ()
 
 		// ---------------------
 
-		function mark_all_as_read(event){
-		 	if(event.target.id == "mark_as_read_button")
+		function mark_all_as_read( event ) {
+		 	if ( event.target.id == "mark_as_read_button" )
 		 	{
 		 		let nm_unread_notices = document.querySelectorAll("#nm_container_unread .nm-common");
-		 		if(nm_unread_notices.length > 0)
+		 		if ( nm_unread_notices.length > 0 )
 		 		{
 		 			const nm_hashes = [];
-		 			for( let nm_notice_idx=0; nm_notice_idx < nm_unread_notices.length; nm_notice_idx++)
+		 			for( let nm_notice_idx = 0; nm_notice_idx < nm_unread_notices.length; nm_notice_idx++)
 		 			{
 		 				let nm_notice = nm_unread_notices[nm_notice_idx];
-		 				nm_hashes.push(MD5(nm_notice.innerHTML));
+		 				nm_hashes.push( MD5( nm_notice.innerHTML ) );
 		 				nm_notice.classList.add("nm-seen");
 		 				nm_container_all.appendChild(nm_notice);
 		 			}
@@ -90,7 +90,7 @@ window.addEventListener('load', function ()
                                         	$.ajax({
                                          	type: "POST",
                                          	url: ajaxurl,
-                                         	data: {action: "update_notification_option", 'nm_notices_hash_data': nm_hashes},
+                                         	data: { action: "update_notification_option", 'nm_notices_hash_data': nm_hashes },
                                          	});
                                  	});
 		 		}
@@ -98,37 +98,37 @@ window.addEventListener('load', function ()
 		}
 
 
-		function nm_all_notices_count(){
+		function nm_all_notices_count() {
 			return document.getElementsByClassName("nm-common").length;
 		}
 
-		function nm_unread_notices_count(){
+		function nm_unread_notices_count() {
 			return nm_all_notices_count() - document.getElementsByClassName("nm-seen").length;
 		}
 
-		function refresh_no_notice_state(){
-			if(document.getElementById("nm_container_all").style.display == "none" || document.getElementById("nm_container_all").style.display == ""){
+		function refresh_no_notice_state() {
+			if ( document.getElementById("nm_container_all").style.display == "none" || document.getElementById("nm_container_all").style.display == "" ) {
 				document.getElementById("no_new_notifications").style.display = nm_unread_notices_count()==0 ? "block" : "none";
 			}
-			else{
+			else {
 				document.getElementById("no_new_notifications").style.display = nm_all_notices_count()==0 ? "block" : "none";
 			}
 		}
 
-		function refresh_notification_numbers(){
-			var nm_count_text =  nm_unread_notices_count() > 0 ? 'Notifications <span id="nm_display_notification_number">' + nm_unread_notices_count() + '</span>' : 'Notifications';
+		function refresh_notification_numbers() {
+			let nm_count_text =  nm_unread_notices_count() > 0 ? 'Notifications <span id="nm_display_notification_number">' + nm_unread_notices_count() + '</span>' : 'Notifications';
 			document.getElementById("notification-count").innerHTML = nm_count_text;
 			document.getElementById("nm_count_unread").innerHTML = nm_unread_notices_count();
 			document.getElementById("nm_count_all").innerHTML = nm_all_notices_count();
 		}
 
-		function nm_alert_for_notice(){
-			if(nm_unread_notices_count() != 0){
+		function nm_alert_for_notice() {
+			if( nm_unread_notices_count() != 0 ) {
 				document.getElementById("nm_notice_alert_box").classList.add("nm_alert_animation");
 			}
 		}
 
-		nm_container.addEventListener("click", function(event) {mark_all_as_read(event);refresh_no_notice_state();setTimeout(refresh_notification_numbers, 300);})
+		nm_container.addEventListener("click", function(event) { mark_all_as_read(event); refresh_no_notice_state(); setTimeout(refresh_notification_numbers, 300); })
 		refresh_no_notice_state();
 		refresh_notification_numbers();
 		setTimeout(nm_alert_for_notice, 15000);
@@ -136,15 +136,15 @@ window.addEventListener('load', function ()
 
 		// ----------------------
 
-		function nm_container_mousevent_css(nm_visiblity, nm_display){
+		function nm_container_mousevent_css( nm_visiblity, nm_display ) {
 			nm_container.style.visibility = nm_visiblity;
 			nm_container.style.display = nm_display;
 		}
 
 		let admin_notification = document.getElementById("wp-admin-bar-notification-manager");
-		admin_notification.addEventListener("mouseover", function(event) {nm_container_mousevent_css("visible","block");})
-		admin_notification.addEventListener("mouseout", function(event) {nm_container_mousevent_css("none","none");})
-		nm_container.addEventListener("mouseover", function(event) {nm_container_mousevent_css("visible","block");})
-		nm_container.addEventListener("mouseout", function(event) {nm_container_mousevent_css("none","none");})
+		admin_notification.addEventListener("mouseover", function(event) { nm_container_mousevent_css("visible","block"); })
+		admin_notification.addEventListener("mouseout", function(event) { nm_container_mousevent_css("none","none"); })
+		nm_container.addEventListener("mouseover", function(event) { nm_container_mousevent_css("visible","block"); })
+		nm_container.addEventListener("mouseout", function(event) { nm_container_mousevent_css("none","none"); })
 	}
 )
--- notification-manager.php
+++ notification-manager.php
@@ -10,15 +10,15 @@ Author URI: https://blogvault.net/
  */
 
 
-if (!defined('ABSPATH')) exit;
+if ( ! defined('ABSPATH') ) exit;
 
 // delete_option("nm_hash_of_read_notices");
-function nm_create_menu($wp_admin_bar){
+function nm_create_menu( $wp_admin_bar ) {
 	$wp_admin_bar->add_node(
 		array(
 			'id' => 'notification-manager',
 			'parent' => 'top-secondary',
-			'title' => '<div id="notification-count" style="text-align:right" class="notification-count"> Notifications </div>',
+			'title' => '<div id="notification-count" style="text-align:right" class="notification-count">Notifications</div>',
 			'href' => false,
 		)
 	);
@@ -27,23 +27,23 @@ function nm_create_menu($wp_admin_bar){
 	?>
 	<div id="nm_container">
 		<div id="nm_container_top">
-			<button onClick='nm_menu_toggle(true)' class="nm_menu_button" id="nm_unread" >Unread<div id="nm_count_unread"></div></button>
-			<button onClick='nm_menu_toggle(false)' class="nm_menu_button" id="nm_all" >All<div id="nm_count_all"></div></button>
-			<button onClick='nm_menu_toggle(true)' id="mark_as_read_button"> Mark all as read </button>
+			<button onClick='nm_menu_toggle(true)' class="nm_menu_button" id="nm_unread">Unread<div id="nm_count_unread"></div></button>
+			<button onClick='nm_menu_toggle(false)' class="nm_menu_button" id="nm_all">All<div id="nm_count_all"></div></button>
+			<button onClick='nm_menu_toggle(true)' id="mark_as_read_button">Mark all as read</button>
 		</div>
 		<hr>
 		<div id="nm_container_bottom">
-			<h3 id="no_new_notifications"> No New Notifications </h3>
+			<h3 id="no_new_notifications">No New Notifications</h3>
 			<div id="nm_container_unread"></div>
 			<div id="nm_container_all"></div>
 		</div>
 	</div>
 	<script>
-	function nm_hide(){
+	function nm_hide() {
 		document.getElementById("nm_notice_alert_box").style.visibility = "hidden";
 	}
 
-	function nm_menu_toggle(nm_toggle){
+	function nm_menu_toggle( nm_toggle ) {
 		document.getElementById("nm_container_all").style.display = nm_toggle ? "none" : "block";
 		document.getElementById("mark_as_read_button").style.display = nm_toggle ? "block" : "none";
 		document.getElementById("nm_all").style.color = nm_toggle ? "#a8aaac" : "black";
@@ -60,18 +60,18 @@ function nm_create_menu($wp_admin_bar){
 
 add_action('admin_bar_menu', 'nm_create_menu', 999);
 
-function nm_enqueue_notice_data(){
+function nm_enqueue_notice_data() {
 	add_option('nm_hash_of_read_notices', array());
 	$nm_hash_of_read_notices = get_option('nm_hash_of_read_notices');
-	wp_enqueue_style('nm_menu',plugin_dir_url(__FILE__) . 'css/nm_menu.css');
+	wp_enqueue_style('nm_menu', plugin_dir_url(__FILE__) . 'css/nm_menu.css');
 	wp_enqueue_script( 'nm_notice_data', plugin_dir_url( __FILE__ ) . 'js/nm_notice_data.js', [], false, true );
 	wp_localize_script('nm_notice_data', 'nm_hash_of_read_notices', $nm_hash_of_read_notices);
 }
 
 add_action('admin_enqueue_scripts', 'nm_enqueue_notice_data');
 
-function nm_add_hash_of_notices_to_db(){
-	if(isset($_REQUEST)){
+function nm_add_hash_of_notices_to_db() {
+	if( isset( $_REQUEST ) ) {
 		$nm_notices_hash_data = $_REQUEST['nm_notices_hash_data'];
 		$nm_hash_of_read_notices = get_option('nm_hash_of_read_notices');
 		$nm_hash_of_read_notices = array_merge($nm_hash_of_read_notices, $nm_notices_hash_data);
